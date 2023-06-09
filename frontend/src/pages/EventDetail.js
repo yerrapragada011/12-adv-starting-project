4dcf9a190b4343bff0b7cabd3657c89f85f08b19
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-09 16:00:13 -0400

    The useRouteLoaderData() Hook & Accessing Data From Other Routes

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,16 +1,16 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useRouteLoaderData, json } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
 
   return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
-export async function loader({request, params}) {
+export async function loader({ request, params }) {
   const id = params.eventId;
 
   const response = await fetch('http://localhost:8080/events/' + id);
